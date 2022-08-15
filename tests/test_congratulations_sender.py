71488dda21a517fb02ce1ef1797d946b555807c4
Ivan <dualking1991@gmail.com>
2022-08-15 12:15:46 +0400

    img

--- tests/test_congratulations_sender.py
+++ tests/test_congratulations_sender.py
@@ -1,7 +1,7 @@
 # Неясен путь реализации. Протестирован вручную (см.jpg-файлы)
 
-import pytest
-from congratulations_sender import send_congratulations
+# import pytest
+# from congratulations_sender import send_congratulations
 
 
 # class TestTasksSuite:
